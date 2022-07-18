16bedaca582194d708506b50a40ec092272bfbc1
lfdfhl <214655371@qq.com>
2022-07-18 23:03:37 +0800

    commit 4

--- src/main/java/com/cn/Test.java
+++ src/main/java/com/cn/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("Hello Git");
         System.out.println("Hello Java");
         System.out.println("Hello GitHub");
+        System.out.println("Hello game");
     }
 }
